588c84cb0cd6132b7b3e9daae1d260acb6355aa4
Bartłomiej Smaciarz <bartlomiej.smaciarz@gmail.com>
2019-09-30 12:35:32 +0200

    #97 - Udemy
    This reverts commit c3e7f69195da4fe5930fa217701505293af07968.

---
+++ PortalRandkowy.SPA/src/app/app.module.ts
@@ -0,0 +1,87 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { JwtModule } from '@auth0/angular-jwt';
+import { RouterModule } from '@angular/router';
+import { BsDropdownModule, TabsModule, BsDatepickerModule } from 'ngx-bootstrap';
+import { NgxGalleryModule } from 'ngx-gallery';
+import { FileUploadModule } from 'ng2-file-upload';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TimeAgoPipe } from 'time-ago-pipe';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { AlertifyService } from './_services/alertify.service';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { LikesComponent } from './likes/likes.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { UserCardComponent } from './users/user-card/user-card.component';
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
+import { UserDetailResolver } from './_resolvers/user-detail.resolver';
+import { UserListResolver } from './_resolvers/user-list.resolver';
+import { UserEditComponent } from './users/user-edit/user-edit.component';
+import { UserEditResolver } from './_resolvers/user-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { PhotosComponent } from './users/photos/photos.component';
+
+export function tokenGetter() {
+   return localStorage.getItem('token');
+}
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      NavComponent,
+      HomeComponent,
+      RegisterComponent,
+      UserListComponent,
+      LikesComponent,
+      MessagesComponent,
+      UserCardComponent,
+      UserDetailComponent,
+      UserEditComponent,
+      PhotosComponent,
+      TimeAgoPipe
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      FormsModule,
+      JwtModule.forRoot({
+         config: {
+            tokenGetter,
+            whitelistedDomains: ['localhost:51634'],
+            blacklistedRoutes: ['localhost:51634/api/auth']
+         }
+      }),
+      RouterModule.forRoot(appRoutes),
+      BsDropdownModule.forRoot(),
+      TabsModule.forRoot(),
+      NgxGalleryModule,
+      FileUploadModule,
+      ReactiveFormsModule,
+      BrowserAnimationsModule,
+      BsDatepickerModule.forRoot(),
+   ],
+   providers: [
+      AuthService,
+      AlertifyService,
+      AuthGuard,
+      ErrorInterceptorProvider,
+      UserDetailResolver,
+      UserListResolver,
+      UserEditResolver,
+      PreventUnsavedChanges
+   ],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
